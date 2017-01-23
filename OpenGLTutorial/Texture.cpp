402338e284062c0d8659f16a493333ec9cd38bb8
Adrien Perinet <aperinet@mines.edu>
2017-01-23 12:09:56 -0700

    Added new class (incomplete)

---
+++ OpenGLTutorial/Texture.cpp
@@ -0,0 +1,24 @@
+#include "Texture.h"
+#include "stb_image.h"
+
+Texture::Texture(const std::string& fileName)
+{
+	unsigned int width, height, numComponents;
+	char* imageData = stbi_load(fileName.c_str(), &width, &height, &numComponents, 4);
+
+	glGenTextures(1, &m_texture);
+	glBindTexture(GL_TEXTURE_2D, m_texture);
+
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTextureParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTextureParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	stbi_image_free(imageData);
+}
+
+
+Texture::~Texture()
+{
+}
---
+++ OpenGLTutorial/Texture.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <GL/glew.h>
+
+class Texture
+{
+public:
+	Texture(const std::string& fileName);
+
+	void bind(unsigned int unit);
+
+	virtual ~Texture();
+private:
+	GLuint m_texture;
+};
+
